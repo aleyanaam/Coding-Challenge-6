ea65900d4d342448f7d423de029c2b91c92cc064
Aleyana McLeod <aleyanam@usf.edu>
2024-09-22 23:23:58 -0400

    Create displayEmployeeShifts function

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -10,4 +10,20 @@ const employees = [
     { name: 'Emily', shifts: [{ day: 'Friday', hours: 8 }] }
     
     ];
-    
+
+//Task 2 
+function displayEmployeeShifts(employees) {
+    //Map iterates over the employee array
+    return employees.map(employee => {
+    // Checks to see if an employee has no shifts
+        if (employee.shifts.length === 0) {
+            return `${employee.name} has no shifts.`;
+        } else {
+            //If an employee has a shift it is listed with the day and hours as well as the employee name
+            let shiftInfo = employee.shifts.map(shift => `${shift.day}: ${shift.hours} hours`);
+            return (`${employee.name} has the following shifts: ${shiftInfo}`);
+        }
+    }) 
+}
+
+console.log(displayEmployeeShifts(employees));
